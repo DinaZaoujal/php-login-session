6ab08bb2f40f15633188d40a50159c4114255a70
DinaZaoujal <r1036827@student.thomasmore.be>
2025-11-27 16:40:28 +0100

    fix:PDO ERRORMODE_EXCEPTION toegevoegd om de verbindingsfouten te voorkomen

--- login.php
+++ login.php
@@ -1,8 +1,40 @@
 <?php // login.php 
 
-session_start();
-if (!empty($_SESSION))
+//session_start();
+//if (!empty($_SESSION))
+function canLogin($p_email , $p_password){
+$conn = new PDO('mysql:host=localhost;dbname=`php webshop`', "root", "");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+$statement=$conn->prepare("SELECT*FROM user WHERE email=:email");
+
+$statement->execute();
+
+$user=$statement->fetch(PDO::FETCH_ASSOC);
+if (password_verify($p_password,$user['password'])){
+    return true;
+
+}else{
+    return false;
+}
+}
+
+if (!empty($_POST)){
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+
+    if(canLogin($email,$password)){
+        $salt="342334535DFE3ZTD5";
+        $cookieVal=$email . ",". md5($email.$salt);
+        setcookie("loggedin",$cookieVal, time()+60*60*24*30,"/","",true,true);
+        
+        header('Location: index.php');
+        exit;
+
+    }else{
+        $error= "Sorry, we can't log in with that email adress and password. Can you try again?";
+    }
+}
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,9 +44,10 @@ if (!empty($_SESSION))
 </head>
 <body>
  <h1>log-in</h1>
- <?php if (!empty($error)): ?>
+ <?php if (isset($error)): ?>
 <p style="color:red;"><?php echo ($error); ?></p>
 <?php endif; ?>
+<div class="form_field">
 <form method="post">
 <label>E-mail:</label><br>
 <input type="email" name="email" required><br><br>
@@ -25,6 +58,8 @@ if (!empty($_SESSION))
 
 </form>
 <p>geen account?<a href="register.php">Registreer hier</a></p>
-</form>   
+</div>
+</form>  
+
 </body>
 </html>
