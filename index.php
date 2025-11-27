4e879857cc26dd40f7a23ebbe6eb6451b9db746e
DinaZaoujal <r1036827@student.thomasmore.be>
2025-11-27 13:26:39 +0100

    fix: project structuur bewerken

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php // index.php ?>
+<?php // index.php 
+include_once(__DIR__."/data.inc.php");
+
+$cookie=$_COOKIE['loggedin']?? null;
+$salt="342334535DFE3ZTD5";
+if(!$cookie){
+    header("location: login.php");
+    exit;
+}
+$frags = explode(",",$cookie);
+
+if(!isset($frags[1])||$frags[1]!==md5($frags[0].$salt)){
+    header("location:login.php");
+    exit;
+}
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechGadgets</title>
+</head>
+<body>
+    
+</body>
+</html>
--- logout.php
+++ logout.php
@@ -1,1 +1,6 @@
-<?php // logout.php ?>
+<?php // logout.php
+session_start();
+$_SESSION = [];
+session_destroy();
+header("Location: login.php")
+?>
