6b80fee625a8ead4e594df06c13be42b786a24ba
DinaZaoujal <r1036827@student.thomasmore.be>
2025-12-09 15:08:04 +0100

    error: product.php

---
+++ product.php
@@ -0,0 +1,25 @@
+<?php
+require_once"Classes/product.php";
+$products= Product::getAll();
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Producten</title>
+
+</head>
+<body>
+  <h1> Producten Lijst</h1>  
+  <?php foreach($products as $p); ?>
+  <div>
+    <h2><?php echo $p['name']; ?></h2>
+    <p><?php echo $p['description']; ?></p>
+    <p>Prijs: €<?php echo $p['price']; ?></p>
+    <img src="uploads/<?php echo $p['image']; ?>" width="150">
+  </div>
+  <?php endforeach; ?>
+
+</body>
+</html>
